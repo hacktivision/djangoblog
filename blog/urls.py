894c83ec40a446b488389978d972013218efba89
Nick <komplydesign@gmail.com>
2021-02-28 20:58:58 +0700

    Create template pages

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import index
+from .views import posts_list, post_detail
 
 urlpatterns = [
-    path('', index)
+    path('', posts_list, name='posts_list'),
+    path('<int:post_id>', post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 from .models import Post
 
-def index(request):
-    posts = Post.objects.all()
-    return HttpResponse([f'<h2>{post.title}</h2><p>{post.text}</p><hr>' for post in posts])
+def posts_list(request):
+    posts = Post.objects.filter(published_date__lt=timezone.now()).order_by('published_date')
+    context = {
+        'title': 'My posts',
+        'posts': posts,
+        'count': posts.count()
+    }
+    return render(request, 'blog/posts.html', context=context)
+
+def post_detail(request, post_id):
+    post = Post.objects.get(pk=post_id)
+    context = {
+        'title': post.title,
+        'text': posts.text,
+    }
+    return render(request, 'blog/post_detail.html', context=context)
+    
     
