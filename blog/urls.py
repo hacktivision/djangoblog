6fdda79fc0248e0fb4e8dfb05cbc302b1b3b2fea
Nick <komplydesign@gmail.com>
2021-03-01 11:56:17 +0700

    Blog priject is done!

--- blog/urls.py
+++ blog/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from .views import post_list, post_detail, post_new
+from .views import post_list, post_detail, post_new, post_edit
 
 urlpatterns = [
     path('', post_list, name='post_list'),
     path('post/<int:pk>/', post_detail, name='post_detail'),
     path('post/new/', post_new, name='post_new'),
+    path('post/<int:pk>/edit/', post_edit, name='post_edit'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.utils import timezone
@@ -22,13 +22,38 @@ def post_detail(request, pk):
     }
     return render(request, 'blog/post_detail.html', context=context)
 
-
+    
 def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    
     form = PostForm()
     context = {
         'title': 'Добовление поста',
         'form': form,
     }
     return render(request, 'blog/post_new.html', context=context)
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
     
-    
+    form = PostForm(instance=post)
+    context = {
+        'title': 'Добовление поста',
+        'form': form,
+    }
+    return render(request, 'blog/post_new.html', context=context)
